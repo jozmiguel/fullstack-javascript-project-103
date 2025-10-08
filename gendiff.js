96b3bce51fa954c2fce511957cb29737adceda0c
Jozmiguel <jozmiguelcardenas@gmail.com>
2025-10-07 21:33:29 -0500

    Implementa comparación de archivos JSON planos en gendiff

--- gendiff.js
+++ gendiff.js
@@ -12,7 +12,9 @@ program
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format')
   .action((filepath1, filepath2, options) => {
-    genDiff(filepath1, filepath2);
+    const diff = genDiff(filepath1, filepath2); 
+    console.log(diff); 
   });
 
-program.parse(process.argv);
+program.parse(process.argv); 
+
--- src/index.js
+++ src/index.js
@@ -1,25 +1,36 @@
+// src/index.js
 import fs from 'fs';
 import path from 'path';
+import _ from 'lodash';
 
-// Función principal: lee dos archivos JSON y los analiza
+const readFile = (filepath) => {
+  const fullPath = path.resolve(process.cwd(), filepath);
+  const data = fs.readFileSync(fullPath, 'utf-8');
+  return JSON.parse(data);
+};
+
 export default function genDiff(filepath1, filepath2) {
-  // Convertir las rutas a rutas absolutas
-  const absolutePath1 = path.resolve(process.cwd(), filepath1);
-  const absolutePath2 = path.resolve(process.cwd(), filepath2);
+  const data1 = readFile(filepath1);
+  const data2 = readFile(filepath2);
 
-  // Leer los archivos (sincrónicamente)
-  const fileContent1 = fs.readFileSync(absolutePath1, 'utf-8');
-  const fileContent2 = fs.readFileSync(absolutePath2, 'utf-8');
+  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
 
-  // Analizar (convertir JSON en objetos)
-  const data1 = JSON.parse(fileContent1);
-  const data2 = JSON.parse(fileContent2);
+  const lines = keys.map((key) => {
+    if (!Object.hasOwn(data2, key)) {
+      return `  - ${key}: ${data1[key]}`;
+    }
+    if (!Object.hasOwn(data1, key)) {
+      return `  + ${key}: ${data2[key]}`;
+    }
+    if (data1[key] !== data2[key]) {
+      return [
+        `  - ${key}: ${data1[key]}`,
+        `  + ${key}: ${data2[key]}`,
+      ].join('\n');
+    }
+    return `    ${key}: ${data1[key]}`;
+  });
 
-  // Mostrar en consola los resultados (por ahora)
-  console.log('Archivo 1:', data1);
-  console.log('Archivo 2:', data2);
-
-  // Por ahora devolvemos algo simple (luego haremos la comparación)
-  return '';
+  return `{\n${lines.join('\n')}\n}`;
 }
 
